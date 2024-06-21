098d39268606ae2855f4191eb7719f0edc3906aa
DAVID RAJ <hackerraj727@gmail.com>
2024-06-22 00:50:02 +0600

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "[ 🐐 | Goat Bot V2 ]";
+const doNotDelete = "[ ᴅᴀᴠɪᴅ ʀᴀᴊ ]";
 /**
 * @author NTKhang
 * @author: do not delete it
